8149966b2deb42c8b6a852c9b445a4f4acd9b5de
koorosh <koorosh.akl@protonmail.com>
2024-09-30 07:38:42 +0300

    sycn

--- generate-pages.scm
+++ generate-pages.scm
@@ -2,8 +2,42 @@
 # -*- mode: scheme; coding: utf-8 -*-
 exec guile -L /home/koorosh/proj/webXRpro/src -e main -s "$0" "$@"
 !#
+(use-modules (ice-9 match)
+	     (ice-9 popen)
+	     (ice-9 textual-ports)
+	     (srfi srfi-26)
+	     (artanis artanis)
+	     (config))
 
-(use-modules (ice-9 match))
+(define od (make-parameter "./output/"))
+(define-inlinable (tpl->html-string tpl) (format #f "<!DOCTYPE html>\n~a" (tpl->html tpl)))
+(define-inlinable (file-in-output name)  (format #f "./~a/~a" (od) name))
+
+(define (tpl->html-file)
+  (parameterize ([static-gen #t])
+    (map (match-lambda
+	   [(name pager)
+	    (with-output-to-file (write (tpl->html-string (pager)) p))])
+	 all-pages)))
+
+(define (copy-statics)
+  (let* ([files-in-pub (map car (cddr (file-system-tree "./pub/")))]
+	 [pipe (open-input-pipe (format #f "cp -r ./pub/*  ./~a" (od)) )])
+    (display (get-string-all pipe))
+    (close-pipe pipe)));;done
+
+(define (gen-output)
+  (copy-statics)
+  (tpl->html-file ))
+
 (define (main args)
-  (match args
-    [(output )]))
+  (match (cdr args)
+    [() (gen-output)]
+    [(output)
+     (when (or (not (file-exists?       output))
+	       (not (file-is-directory? output)))
+       (format (current-error-port) "~a does not exist or is not a directory\n" output)
+       (exit -1))
+     (parameterize ([od output])
+       (gen-output))]
+    [else (display "name one directory for output of pages") (exit -2)]))
--- run.scm
+++ run.scm
@@ -1,11 +1,12 @@
 #! /bin/sh
 # -*- mode: scheme; coding: utf-8 -*-
-exec guile -L /home/koorosh/proj/webXRpro/src -e main -s "$0" "$@"
+exec guile -L ./webXRpro/src -e main -s "$0" "$@"
 !#
 
 (use-modules (ice-9 match)
 	     (mapping)
 	     (system repl server))
+
 (define (init-debug spath)
   (let ([s (make-unix-domain-server-socket #:path spath)])
     (spawn-server s)))
@@ -19,12 +20,3 @@ exec guile -L /home/koorosh/proj/webXRpro/src -e main -s "$0" "$@"
      (stop-server-and-clients!)
      (delete-file spath)]
     [else (error "wrong argument format to main" arg)]))
-
-;; (define-public (start)
-;;   (init-server #:statics '(css js glb) #:exclude '())
-;;   (get "/" (lambda (rc)
-;;	     (call-with-input-file "/home/koorosh/proj/webXRpro/index.html"
-;;	       get-string-all)))
-;;   (run #:port 8080))
-;; (define (main . a)
-;;   (start))
